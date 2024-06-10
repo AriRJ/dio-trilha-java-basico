ed49f00e783f7356121307b2aa84c4cb53d78afd
Alberto <ari.alberto3@gmail.com>
2024-06-10 13:32:45 -0300

    Calculadora

---
+++ metodos/sistema-smart-tv/src/Calculadora.java
@@ -0,0 +1,10 @@
+public class Calculadora {
+    /**
+    *@param numeroUm
+    *@param numeroDois
+    *@return
+    */
+    public int somar(int numeroUm, int numeroDois){
+        return numeroUm + numeroDois;
+    }
+}
---
+++ metodos/sistema-smart-tv/src/Comentarios.java
@@ -0,0 +1,18 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário de um única linha
+
+        /* Olá,
+         * Eu sou um comentário
+         * que pode ser mais detalhado
+         * quando necessário
+         */
+
+         /**
+          * Essas duas estrelinhas acima
+          é para identificar que você
+          pretende elaborar um comentário
+          a nivel de documentação.
+          */
+    }
+}
